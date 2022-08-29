f6b3ad52d58c1397af4ec7828247922b368228a8
Aniket Maurya <theaniketmaurya@gmail.com>
2022-08-29 11:43:58 +0530

    update version

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="slack_command_bot",
-    version="0.0.0",
+    version="0.0.1",
     description="⚡ With this app you can create a Slack bot and enable interactivity with the Slash Commands.",
     author="Aniket Maurya",
     author_email="aniket@lightning.ai",
-    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url="https://github.com/Lightning-AI/lightning-component-template",
+    url="https://github.com/Lightning-AI/LAI-slack-slash-sommand-bot-Component",
     install_requires=[],
     packages=find_packages(),
 )
